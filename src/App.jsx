615d89f286fb855a82658eb684adac688a53b343
Akash2006h <akashkarki352@gmail.com>
2025-03-09 11:24:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className ="text-3xl font-bold text-green-800" >Hello world</div>
+  
+  )
+}
+
+export default App
